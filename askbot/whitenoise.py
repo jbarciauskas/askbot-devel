68d4378362ae79b35a57ee5e52dd76276f32d2eb
Joel Barciauskas <joel@edx.org>
2017-01-09 17:36:20 -0500

    Move things around a bit

--- askbot/whitenoise.py
+++ askbot/whitenoise.py
@@ -6,11 +6,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
-from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(get_wsgi_application())
